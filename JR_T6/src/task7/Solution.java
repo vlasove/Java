4cb368e74132631efea5f066d05c7f900ff82dd8
EvgenyVl <evgeny_vlasov@yahoo.com>
2020-11-19 21:57:19 +0300

    Second batch done

---
+++ JR_T6/src/task7/Solution.java
@@ -0,0 +1,33 @@
+package task7;
+
+public class Solution {
+    public static void main(String[] args) {
+        print(1);
+    }
+
+    public static void print(byte b) {
+        System.out.println("byte");
+        System.out.println("Я буду Java прогером!");
+    }
+
+    public static void print(double d) {
+        System.out.println("double");
+        System.out.println("Я буду Java прогером!");
+    }
+
+    public static void print(long l) {
+        System.out.println("long");
+        System.out.println("Я буду Java прогером!");
+    }
+
+    public static void print(float f) {
+        System.out.println("float");
+        System.out.println("Я буду Java прогером!");
+    }
+
+    public static void print(char c) {
+        System.out.println("char");
+        System.out.println("Я буду Java прогером!");
+    }
+}
+
