e05245f68c13a9a8f834d6b476ac71c5dfc45d1b
EvgenyVl <evgeny_vlasov@yahoo.com>
2020-11-30 17:29:34 +0300

    Started Task3: can not find problem

---
+++ JR_T8/src/task3/Dishes.java
@@ -0,0 +1,13 @@
+package task3;
+
+public class Dishes {
+    private byte tableNumber;
+
+    public Dishes(byte tableNumber) {
+        this.tableNumber = tableNumber;
+    }
+
+    public byte getTableNumber() {
+        return tableNumber;
+    }
+}
