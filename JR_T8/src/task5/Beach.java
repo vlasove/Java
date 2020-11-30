140d2edc02e8696d6b443030b70ffbe3d4ade0e2
EvgenyVl <evgeny_vlasov@yahoo.com>
2020-11-30 23:19:57 +0300

    Add task6

--- JR_T8/src/task5/Beach.java
+++ JR_T8/src/task5/Beach.java
@@ -1,7 +1,7 @@
 package task5;
 
 
-public class Beach {
+public class Beach implements  Comparable<Beach>{
     private String name;      //название
     private float distance;   //расстояние
     private int quality;    //качество
@@ -12,32 +12,37 @@ public class Beach {
         this.quality = quality;
     }
 
-    public String getName() {
+    public synchronized String getName() {
         return name;
     }
 
-    public void setName(String name) {
+    public synchronized void setName(String name) {
         this.name = name;
     }
 
-    public float getDistance() {
+    public synchronized float getDistance() {
         return distance;
     }
 
-    public void setDistance(float distance) {
+    public synchronized void setDistance(float distance) {
         this.distance = distance;
     }
 
-    public int getQuality() {
+    public synchronized int getQuality() {
         return quality;
     }
 
-    public void setQuality(int quality) {
+    public synchronized void setQuality(int quality) {
         this.quality = quality;
     }
 
-    public static void main(String[] args) {
+    public synchronized static void main(String[] args) {
 
+    }
+
+
+    public synchronized int compareTo(Beach b) {
+        return (int) (this.quality - this.distance) - (int)(b.quality - b.distance);
     }
 }
 
