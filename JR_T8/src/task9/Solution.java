28cb1597085cda554a6002972d7ba82bc7256ae9
EvgenyVl <evgeny_vlasov@yahoo.com>
2020-12-01 14:29:45 +0300

    Task9 Done

--- JR_T8/src/task9/Solution.java
+++ JR_T8/src/task9/Solution.java
@@ -15,9 +15,56 @@ public class Solution {
     public static List<String> forRemoveLines = new ArrayList<String>();
 
     public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String firstFileName = null;
+        String secondFileName = null;
+        try {
+            firstFileName = reader.readLine();
+            secondFileName = reader.readLine();
+            reader.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try{
+            reader = new BufferedReader(new FileReader(firstFileName));
+            String line = reader.readLine();
+            while (line != null){
+                allLines.add(line);
+                line = reader.readLine();;
+            }
+            reader.close();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        try{
+            reader = new BufferedReader(new FileReader(secondFileName));
+            String line = reader.readLine();
+            while(line != null){
+                forRemoveLines.add(line);
+                line = reader.readLine();;
+            }
+            reader.close();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        try {
+            Solution s = new Solution();
+            s.joinData();
+        } catch (CorruptedDataException ce){
+            ce.printStackTrace();
+        }
     }
 
     public void joinData() throws CorruptedDataException {
-
+        if(allLines.containsAll(forRemoveLines)){
+            for (String line : forRemoveLines){
+                allLines.remove(line);
+            }
+        } else {
+            allLines.clear();
+            throw  new CorruptedDataException();
+        }
     }
 }
