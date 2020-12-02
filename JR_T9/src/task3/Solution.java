03e39e8893ecb7bca23d05c9199b8956a02f205b
EvgenyVl <evgeny_vlasov@yahoo.com>
2020-12-03 00:06:18 +0300

    Formatted

--- JR_T9/src/task3/Solution.java
+++ JR_T9/src/task3/Solution.java
@@ -13,7 +13,7 @@ public class Solution {
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String fileName = reader.readLine();
-        if(args.length >0) {
+        if (args.length > 0) {
             if (args[0].equals("-c")) {
                 String productName = args[1];
                 String price = args[2];
@@ -24,22 +24,23 @@ public class Solution {
                 int maxId = findMaxId(fileName);
                 appendToFile(stream, maxId, productName, price, quantity);
                 stream.close();
-            } else if(args[0].equals("-u")){
+            } else if (args[0].equals("-u")) {
                 //TODO: add update functionality
-            } else if(args[0].equals("-d")){
+            } else if (args[0].equals("-d")) {
                 //TODO: add delete functionality
             }
         }
         reader.close();
 
     }
-    public static int findMaxId(String fileName) throws Exception{
+
+    public static int findMaxId(String fileName) throws Exception {
         int maxId = 0;
         BufferedReader stream = new BufferedReader(new FileReader(fileName));
         String line = null, last = null;
         while ((line = stream.readLine()) != null) {
             int curId = Integer.parseInt(line.substring(0, 8).trim());
-            if(curId > maxId){
+            if (curId > maxId) {
                 maxId = curId;
             }
         }
