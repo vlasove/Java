4401a394b6dfd2928fc0b82f34c843828e30b7c6
EvgenyVl <evgeny_vlasov@yahoo.com>
2020-12-02 23:55:59 +0300

    Task2 done

--- JR_T9/src/task2/Solution.java
+++ JR_T9/src/task2/Solution.java
@@ -12,9 +12,39 @@ public class Solution {
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String fileName = reader.readLine();
+        if(args.length >0) {
+            if (args[0].equals("-c")) {
+                String productName = args[1];
+                String price = args[2];
+                String quantity = args[3];
+                FileOutputStream stream = new FileOutputStream(fileName, true);
 
-        if(args[0].equals("-c")){
 
+                int maxId = findMaxId(fileName);
+                appendToFile(stream, maxId, productName, price, quantity);
+                stream.close();
+            }
         }
+        reader.close();
+
     }
+    public static int findMaxId(String fileName) throws Exception{
+        int maxId = 0;
+        BufferedReader stream = new BufferedReader(new FileReader(fileName));
+        String line = null, last = null;
+        while ((line = stream.readLine()) != null) {
+            int curId = Integer.parseInt(line.substring(0, 8).trim());
+            if(curId > maxId){
+                maxId = curId;
+            }
+        }
+        stream.close();
+        return maxId;
+    }
+
+    public static void appendToFile(FileOutputStream stream, int maxId, String productName, String price, String quantity) throws Exception {
+        String ans = "\n" + String.format("%-8s%-30s%-8s%-4s", ++maxId, productName, price, quantity);
+        stream.write(ans.getBytes());
+    }
+
 }
