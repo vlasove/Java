bdfe5df881c34acd81db791c9d471ccab83ef95c
EvgenyVl <evgeny_vlasov@yahoo.com>
2020-12-02 14:30:39 +0300

    Shit Lec12

---
+++ JR_T9/src/lec12/Solution.java
@@ -0,0 +1,43 @@
+package lec12;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        new Thread(new DecoratorRunnableImpl(new DecoratorMyRunnableImpl(new RunnableImpl()))).start();
+    }
+
+    public static class RunnableImpl implements Runnable {
+        @Override
+        public void run() {
+            System.out.println("RunnableImpl body");
+        }
+    }
+
+    public static class DecoratorRunnableImpl implements Runnable {
+        private Runnable component;
+
+        public DecoratorRunnableImpl(Runnable component) {
+            this.component = component;
+        }
+
+        @Override
+        public void run() {
+            System.out.print("DecoratorRunnableImpl body ");
+            component.run();
+        }
+    }
+
+    public static class DecoratorMyRunnableImpl implements  Runnable{
+        private Runnable component;
+
+        private DecoratorMyRunnableImpl(Runnable component){
+            this.component = component;
+        }
+        @Override
+        public void run(){
+            System.out.print("DecoratorMyRunnableImpl body ");
+            component.run();
+        }
+    }
+
+}
