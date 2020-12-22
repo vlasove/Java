1d4ce8360a9d1e599bbc8c94d450b9058fcde587
EvgenyVl <evgeny_vlasov@yahoo.com>
2020-12-22 22:36:35 +0300

    Need to solve Lec18

--- JR_T10/src/lections/lec18/Solution.java
+++ JR_T10/src/lections/lec18/Solution.java
@@ -15,43 +15,85 @@ public class Solution {
         String firstFileName = new String();
         String secondFileName = new String();
         BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
-        try{
+        try {
             firstFileName = consoleReader.readLine();
             secondFileName = consoleReader.readLine();
             consoleReader.close();
-        } catch (IOException ioe){
+        } catch (IOException ioe) {
             ioe.printStackTrace();
         }
         List<String> firstFileStrings = readFile(firstFileName);
         List<String> secondFileStrings = readFile(secondFileName);
-
-
-        System.out.println("First file:");
-        printList(firstFileStrings);
-
-        System.out.println("Second file:");
-        printList(secondFileStrings);
+        checkDifference(firstFileStrings, secondFileStrings);
+        printLines(lines);
     }
-    public static List<String> readFile(String fileName){
+
+    public static void printLines(List<LineItem> lst) {
+        for (LineItem li : lst) {
+            System.out.print(li.type + " ");
+            System.out.println(li.line);
+        }
+    }
+
+    public static void checkDifference(List<String> first, List<String> second) {
+        int f1ID = 0;
+        int f2ID = 0;
+        while (f1ID < first.size() && f2ID < second.size()) {
+            //printLines(lines);
+            // System.out.println("F1ID:" + f1ID + ", F2ID:" + f2ID);
+
+
+            if (f1ID + 1 < first.size() && first.get(f1ID + 1).equals(second.get(f2ID))) {
+                lines.add(new LineItem(Type.REMOVED, first.get(f1ID)));
+                f1ID++;
+                continue;
+
+            } else if (f2ID + 1 < second.size() && first.get(f1ID).equals(second.get(f2ID + 1))) {
+                lines.add(new LineItem(Type.ADDED, second.get(f2ID)));
+                f2ID++;
+                continue;
+            } else if (first.get(f1ID).equals(second.get(f2ID))) {
+                lines.add(new LineItem(Type.SAME, second.get(f2ID)));
+                f1ID++;
+                f2ID++;
+                continue;
+            } else {
+                break;
+            }
+
+        }
+        if (f2ID == second.size() && f1ID == first.size() - 1) {
+            lines.add(new LineItem(Type.REMOVED, first.get(f1ID)));
+            return;
+        }
+        if (f1ID == first.size() && f2ID == second.size() - 1) {
+            lines.add(new LineItem(Type.ADDED, second.get(f2ID)));
+            return;
+        }
+
+    }
+
+    public static List<String> readFile(String fileName) {
         BufferedReader reader = null;
-        try{
+        try {
             reader = new BufferedReader(new FileReader(fileName));
-        } catch(FileNotFoundException fnfe){
+        } catch (FileNotFoundException fnfe) {
             fnfe.printStackTrace();
         }
         List<String> strings = new ArrayList<>();
-        try{
-            while(reader.ready()){
+        try {
+            while (reader.ready()) {
                 strings.add(reader.readLine());
             }
             reader.close();
-        } catch(IOException ioe){
+        } catch (IOException ioe) {
             ioe.printStackTrace();
         }
         return strings;
     }
-    public static void printList(List<String> lst){
-        for(String row : lst){
+
+    public static void printList(List<String> lst) {
+        for (String row : lst) {
             System.out.println(row);
         }
     }
